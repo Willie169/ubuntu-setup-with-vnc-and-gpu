d6bbd757d863784f33fcce33206882373269ca7c
Willie Shen <cg10250207@gmail.com>
2025-07-22 11:53:21 +0800

    m

---
+++ tmp.sh
@@ -0,0 +1,25 @@
+cat << 'EOF' > ~/.installtmp.sh
+#!/bin/bash
+sudo systemctl --user disable installtmp.service
+rm ~/.config/systemd/user/installtmp.service
+rm -- "$0"
+echo 'test'
+EOF
+chmod +x ~/installtmp.sh
+mkdir -p ~/.config/systemd/user
+cat << EOF > ~/.config/systemd/user/installtmp.service
+[Unit]
+Description=Installation Temporary
+After=network.target
+
+[Service]
+ExecStart="$PREFIX"/.installtmp.sh
+Type=oneshot
+RemainAfterExit=no
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl --user daemon-reload
+systemctl --user enable installtmp.service
+sudo reboot

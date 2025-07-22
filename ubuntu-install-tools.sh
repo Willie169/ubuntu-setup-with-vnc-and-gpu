6431de9765ee4c19c44bc7bac744b8de76736d07
Willie Shen <cg10250207@gmail.com>
2025-07-22 12:08:22 +0800

    m

--- ubuntu-install-tools.sh
+++ ubuntu-install-tools.sh
@@ -1,15 +1,14 @@
 sudo apt update
 sudo apt purge fcitx* -y
-sudo apt install fcitx5 fcitx5-* -y
-im-config # Set to fcitx5. Choose methods in Fcitx 5 Configuration. For Traditional Chinese, choose Chewing
-sudo apt install nano openssh-client openssh-server -y
-sudo systemctl enable ssh
-sudo systemctl start ssh
+sudo apt upgrade -y
+sudo apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus fcitx5 fcitx5-* ffmpeg file flatpak gdb gh ghostscript git gnome-software-plugin-flatpak golang grep libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc mlocate nano neovim nodejs npm openjdk-17-jdk openssh-client openssh-server openssl pandoc perl perl-doc procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget zsh -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+im-config -n fcitx5
+systemctl enable ssh
+systemctl start ssh
 sudo ufw enable
 sudo ufw allow ssh
 ip route
-sudo nano /etc/ssh/sshd_config
-sudo apt upgrade -y && sudo apt install automake bash build-essential bzip2 clang cmake command-not-found curl dbus ffmpeg file gdb gh ghostscript git golang grep libboost-all-dev libeigen3-dev libgsl-dev libssl-dev iproute2 jq make maven mc mlocate neovim nodejs npm openjdk-17-jdk openssl pandoc perl perl-doc procps python3-pip python3-all-dev python3-venv rust-all tar tmux vim wget zsh -y
 cd ~
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xvzf install-tl-unx.tar.gz
@@ -17,8 +16,6 @@ sudo rm install-tl-unx.tar.gz
 cd install-tl-*
 sudo perl install-tl --no-interaction
 cd ~
-echo 'export PATH=/usr/local/texlive/2025/bin/x86_64-linux:$PATH' >> ~/.bashrc
-source ~/.bashrc
 sudo rm -rf install-tl-*
 mkdir -p ~/.config/fontconfig/conf.d
 cat > ~/.config/fontconfig/conf.d/99-texlive.conf << EOF
@@ -124,3 +121,225 @@ sudo wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/No
 sudo wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Regular.otf
 sudo wget https://raw.githubusercontent.com/notofonts/noto-cjk/main/Sans/Mono/NotoSansMonoCJKkr-Bold.otf
 sudo fc-cache -fv
+cd ~
+cat << 'EOF' > ~/profile
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+	. "$HOME/.bashrc"
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export INPUT_METHOD=fcitx
+EOF
+cat << 'EOF' > ~/.bashrc
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+export PATH="$PREFIX/bin:$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PREFIX/glibc/bin:$HOME/.cargo/bin:/opt/TurboVNC/bin:/usr/local/texlive/2025/bin/x86_64-linux/tlmgr:/usr/sbin"
+export CLASSPATH="$PREFIX/lib/antlr-4.13.2-complete.jar"
+export GOROOT="$PREFIX/lib/go"
+export GOPATH="$HOME/go"
+export TORPATH="$PREFIX/etc/tor"
+export PDROOTFS="$PREFIX/var/lib/proot-distro/installed-rootfs"
+export WAYDROID="$HOME/.local/share/waydroid/data/media/0"
+alias antlr4='java -jar $PREFIX/lib/antlr-4.13.2-complete.jar'
+alias grun='java org.antlr.v4.runtime.misc.TestRig'
+alias src=source
+
+gpull() {
+    level="${1:-0}"
+    if [ "$level" -eq 0 ]; then
+        repo_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+        if [ -n "$repo_dir" ]; then
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        else
+            echo "Not in a Git repo."
+        fi
+    else
+        depth=$((level + 1))
+        find . -mindepth "$depth" -maxdepth "$depth" -type d -name .git | while read -r gitdir; do
+            repo_dir=$(dirname "$gitdir")
+            echo "$repo_dir"
+            (cd "$repo_dir" && git pull)
+        done
+    fi
+}
+
+gacp() {
+    git add .
+    git commit -m "$1"
+    git push
+}
+
+gtr() {
+    git tag -a v"$1" -m "Version $1 release"
+    git push origin v"$1"
+    gh release create v"$1" --title "Version $1 release" --notes ''
+}
+EOF
+cat << 'EOF' > ~/.installtmp.sh
+#!/bin/bash
+systemctl --user disable installtmp.service
+rm ~/.config/systemd/user/installtmp.service
+rm -- "$0"
+sudo flatpak install flathub fr.handbrake.ghb -y
+EOF
+chmod +x ~/.installtmp.sh
+mkdir -p ~/.config/systemd/user
+cat << EOF > ~/.config/systemd/user/installtmp.service
+[Unit]
+Description=Installation Temporary
+After=network.target
+
+[Service]
+ExecStart=$(pwd)/.installtmp.sh
+Type=oneshot
+RemainAfterExit=no
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl --user daemon-reload
+systemctl --user enable installtmp.service
+sudo reboot

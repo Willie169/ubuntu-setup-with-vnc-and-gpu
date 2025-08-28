676eefb4ceac8defd34677967d88491aab360bf4
Willie Shen <cg10250207@gmail.com>
2025-08-28 19:22:54 +0800

    o

--- ubuntu-install-tools.sh
+++ ubuntu-install-tools.sh
@@ -5,7 +5,7 @@ sudo apt purge fcitx* -y
 sudo apt upgrade -y
 sudo apt install apt-transport-https automake bash build-essential bzip2 ca-certificates clang cmake command-not-found copyq curl dbus fcitx5 fcitx5-* ffmpeg file flatpak gdb gh ghostscript git glab gnome-software gnome-software-plugin-flatpak golang gpg grep libboost-all-dev libbz2-dev libdb-dev libeigen3-dev libffi-dev libgdbm-compat-dev libgdbm-dev libgsl-dev liblzma-dev libncursesw5-dev libnss3-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libxmlsec1-dev llvm iproute2 jq make maven mc nano neovim nodejs npm openjdk-8-jdk openjdk-11-jdk openjdk-17-jdk openjdk-21-jdk openssh-client openssh-server openssl pandoc perl perl-doc pipx procps python3-pip python3-all-dev python3-venv rust-all software-properties-common tar tk-dev tmux uuid-dev vim wget xz-utils yarn zlib1g-dev zsh -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.asc > /dev/null
-sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt update
 sudo apt install docker-ce -y
 sudo systemctl start docker

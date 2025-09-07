6f5af6dfe217b022bb8b6b4982d90ace0d5bd0b2
Willie Shen <cg10250207@gmail.com>
2025-09-07 21:41:51 +0800

    c

--- install-tools.sh
+++ install-tools.sh
@@ -73,12 +73,18 @@ else
     echo $VERSION_ID
 fi
 )
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg  
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/  
+rm packages.microsoft.gpg  
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'  
+sudo apt update
+sudo apt install code -y
 wget -q https://packages.microsoft.com/config/ubuntu/$OS_VERSION_ID/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
-sudo apt install code powershell dotnet-sdk-8.0 dotnet-runtime-8.0 -y
+sudo apt install powershell dotnet-sdk-8.0 dotnet-runtime-8.0 -y
 cat > ~/.profile << 'EOF' 
 # ~/.profile: executed by the command interpreter for login shells.
 # This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login

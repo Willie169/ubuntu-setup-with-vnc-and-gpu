b9c9e5b3d30c7594a4c17a03e7b90070136379e0
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-10-01 16:23:42 +0800

    Update waydroid.sh

--- waydroid.sh
+++ waydroid.sh
@@ -1,5 +1,9 @@
+cd ~
 sudo apt update
 sudo apt install curl ca-certificates -y
+wget -q http://ftp.us.debian.org/debian/pool/main/l/lxc/lxc_6.0.4-4+b3_amd64.deb
+dpkg -i lxc_6.0.4-4+b3_amd64.deb
+rm lxc_6.0.4-4+b3_amd64.deb
 curl -s https://repo.waydro.id | sudo bash
 sudo apt update
 sudo apt install waydroid -y

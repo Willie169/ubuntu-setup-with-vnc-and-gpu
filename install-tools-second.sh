48af965d0827c6498cb24f9ee9f2f1b7c766f2b7
Willie Shen <cg10250207@gmail.com>
2025-10-01 14:46:11 +0800

    m

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,11 +1,6 @@
 cd ~
 flatpak install flathub com.calibre_ebook.calibre com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
-sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install libgl1:i386 -y
-wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
-sudo dpkg -i steam*.deb
-rm steam*.deb
+
 sudo apt update
 sudo apt install -f -y
 sudo apt full-upgrade -y

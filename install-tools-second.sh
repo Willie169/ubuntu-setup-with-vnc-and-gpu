21905ce9566b7f26464a0b7ae1fa3eb8d307ac02
Willie Shen <cg10250207@gmail.com>
2025-10-01 15:11:37 +0800

    l

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,8 +1,5 @@
 cd ~
 flatpak install flathub com.calibre_ebook.calibre com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
-sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1-mesa-glx:amd64 libgl1-mesa-glx:i386 -y
 wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
 sudo dpkg -i steam*.deb
 rm steam*.deb

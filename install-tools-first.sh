2bf30bc377d58ea4eddd5330b8358aef553d7a9c
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-10-03 14:14:21 +0800

    Update install-tools-first.sh

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -177,6 +177,15 @@ cd tor-browser
 ./start-tor-browser.desktop --detach --headless
 cp start-tor-browser.desktop ~/Desktop
 cd ~
+wget -q http://ftp.us.debian.org/debian/pool/main/libx/libxml2/libxml2-16_2.14.5+dfsg-0.2_amd64.deb
+sudo dpkg -i libxml2-16_2.14.5+dfsg-0.2_amd64.deb
+rm libxml2-16_2.14.5+dfsg-0.2_amd64.deb
+wget -q http://ftp.us.debian.org/debian/pool/main/z/z3/libz3-4_4.13.3-1_amd64.deb
+sudo dpkg -i libz3-4_4.13.3-1_amd64.deb
+rm libz3-4_4.13.3-1_amd64.deb
+wget -q http://ftp.us.debian.org/debian/pool/main/l/llvm-toolchain-19/libllvm19_19.1.7-7_amd64.deb
+sudo dpkg -i libllvm19_19.1.7-7_amd64.deb
+rm libllvm19_19.1.7-7_amd64.deb
 wget -q http://ftp.us.debian.org/debian/pool/main/m/mesa/libosmesa6_25.0.7-2_amd64.deb
 sudo dpkg -i libosmesa6_25.0.7-2_amd64.deb
 rm libosmesa6_25.0.7-2_amd64.deb

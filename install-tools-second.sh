d532d66ce50eb50817e6165dc4f387226976ecef
Willie Shen <cg10250207@gmail.com>
2025-11-04 18:01:10 +0800

    se

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,5 +1,5 @@
 cd ~
-flatpak install flathub com.calibre_ebook.calibre com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
+flatpak install flathub com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
 sudo apt install -f -y
 sudo apt full-upgrade -y
 sudo apt autoremove --purge -y

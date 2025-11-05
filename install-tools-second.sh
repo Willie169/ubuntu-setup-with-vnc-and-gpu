5096d9a1339e101fe947da31b8bfa5a88001f689
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-11-05 11:10:34 +0800

    Install applications and set default browser
    Set Firefox ESR as the default handler for HTTP and HTTPS.

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,3 +1,5 @@
 cd ~
 flatpak install flathub com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
+sed -i '/^\[Default Applications\]/,${s/^x-scheme-handler\/http=.*/x-scheme-handler\/http=firefox-esr.desktop;/}' ~/.config/mimeapps.list
+sed -i '/^\[Default Applications\]/,${s/^x-scheme-handler\/https=.*/x-scheme-handler\/https=firefox-esr.desktop;/}' ~/.config/mimeapps.list
 sudo reboot

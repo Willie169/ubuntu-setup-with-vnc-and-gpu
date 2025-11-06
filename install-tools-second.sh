bb9d462e8883efd190e2212eadf695f3de3698a1
Willie169 <133617026+Willie169@users.noreply.github.com>
2025-11-06 09:27:00 +0800

    Install applications and configure default browser
    Remove existing mimeapps.list and set Firefox as default for HTTP/HTTPS.

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,3 +1,13 @@
 cd ~
 flatpak install flathub com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
+rm ~/.config/mimeapps.list || true
+cat > ~/.config/mimeapps.list <<'EOF'
+[Added Associations]
+x-scheme-handler/http=firefox-esr.desktop;
+x-scheme-handler/https=firefox-esr.desktop;
+
+[Default Applications]
+x-scheme-handler/http=firefox-esr.desktop;
+x-scheme-handler/https=firefox-esr.desktop;
+EOF
 exit

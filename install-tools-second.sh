afdb6c70add8510e4cf550cadea42a6cb8967778
Willie169 <cg10250207@gmail.com>
2026-01-08 15:54:13 +0800

    l

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,5 +1,5 @@
 cd ~
-flatpak install flathub com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
+flatpak install flathub com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client fr.handbrake.ghb io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk net.cozic.joplin_desktop org.chromium.Chromium org.gimp.GIMP org.gnome.Aisleriot org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
 rm ~/.config/mimeapps.list || true
 cat > ~/.config/mimeapps.list <<'EOF'
 [Added Associations]

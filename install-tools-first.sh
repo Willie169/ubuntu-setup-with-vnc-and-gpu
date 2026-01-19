879e4d2d2806b6f90a742a8f84a659bacd083009
Willie Shen <cg10250207@gmail.com>
2026-01-19 16:28:43 +0800

    y

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -238,9 +238,20 @@ rm -r $HOME/cmdline-tools
 cd bin
 echo y | ./sdkmanager "build-tools;30.0.3" "build-tools;35.0.0" "build-tools;36.1.0" "emulator" "ndk;29.0.14206865" "platform-tools" "platforms;android-33" "platforms;android-36" "sources;android-33" "sources;android-36"
 cd ~
+mkdir Android/Sdk/system-images
+mkdir Android/Sdk/system-images/android-33
+mkdir Android/Sdk/system-images/android-33/google_apis_playstore
+mkdir Android/Sdk/system-images/android-36.1
+mkdir Android/Sdk/system-images/android-36.1/google_apis_playstore
 sudo rm /bin/sdkmanager 2>/dev/null || true
 wget https://dl.google.com/android/repository/sys-img/google_apis_playstore/x86_64-33_r09.zip
+unzip x86_64-33_r09.zip
+rm x86_64-33_r09.zip
+mv x84_64 Android/Sdk/system-images/android-33/google_apis_playstore/
 wget https://dl.google.com/android/repository/sys-img/google_apis_playstore/x86_64-36.1_r04.zip
+unzip x86_64-36.1_r04.zip
+rm x86_64-36.1_r04.zip
+mv x84_64 Android/Sdk/system-images/android-36.1/google_apis_playstore/
 gh_latest -A ente-io/ente -t auth* ente-auth-v*-x86_64.deb
 sudo apt install ./ente-auth-v*-x86_64.deb -y
 rm ente-auth-v*-x86_64.deb

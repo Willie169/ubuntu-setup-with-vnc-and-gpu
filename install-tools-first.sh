e2b55afbb377bbfe0fb070835d1badaad8cdd8aa
Willie Shen <cg10250207@gmail.com>
2026-01-19 13:34:44 +0800

    y

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -191,7 +191,7 @@ wget "https://packages.microsoft.com/config/ubuntu/$UBUNTU_VERSION_ID/packages-m
 sudo apt install ./packages-microsoft-prod.deb -y
 rm packages-microsoft-prod.deb
 sudo apt update
-sudo apt install dotnet-sdk-10.0 aspnetcore-runtime-10.0 powershell -y
+sudo apt install dotnet-sdk-10.0 aspnetcore-runtime-10.0 -y
 sudo gpg --homedir /tmp --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo chmod +r /usr/share/keyrings/mono-official-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list

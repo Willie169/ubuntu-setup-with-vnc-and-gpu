9bc65a86aec5ef8c618c218ed95cb0f7aaeaa0f3
Willie Shen <cg10250207@gmail.com>
2026-02-24 11:33:11 +0800

    l

--- xmrig.sh
+++ xmrig.sh
@@ -1,2 +1,11 @@
 cd ~/xmrig
-./xmrig -o pool.supportxmr.com:3333 -u 48j6iQDeCSDeH46gw4dPJnMsa6TQzPa6WJaYbBS9JJucKqg9Mkt5EDe9nSkES3b8u7V6XJfL8neAPAtbEpmV2f4XC7bdbkv -k -x 127.0.0.1:9050 -t $(nproc) --cpu-priority=0
+rm -r .tor 2>/dev/null || true
+mkdir .tor
+cat > .tor/torrc <<'EOF'
+SocksPort 9054
+DataDirectory .tor
+EOF
+tor -f .tor/torrc &
+TORPID=$!
+trap 'kill $TORPID; rm -r ~/xmrig/.tor' EXIT
+./xmrig -o pool.supportxmr.com:3333 -u 48j6iQDeCSDeH46gw4dPJnMsa6TQzPa6WJaYbBS9JJucKqg9Mkt5EDe9nSkES3b8u7V6XJfL8neAPAtbEpmV2f4XC7bdbkv -k -x 127.0.0.1:9054 -t $(nproc) --cpu-priority=0

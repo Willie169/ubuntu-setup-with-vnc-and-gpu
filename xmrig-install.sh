f86eb0f0c6143f313e660e25712796a9ad6b0116
Willie Shen <cg10250207@gmail.com>
2026-02-26 16:40:09 +0800

    l

--- xmrig-install.sh
+++ xmrig-install.sh
@@ -1,7 +1,6 @@
 cd ~
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev tor -y
-git clone https://github.com/xmrig/xmrig.git
-sed -i -e 's/keccak_f800_round(uint32_t st\[25\], const int r)/keccak_f800_round(__private uint32_t *st, const int r)/' -e 's/keccak_f800(uint32_t\* st)/keccak_f800(__private uint32_t *st)/' ~/xmrig/src/backend/opencl/cl/kawpow/kawpow.cl
+git clone https://github.com/Willie169/xmrig.git
 mkdir ~/xmrig/build
 cd ~/xmrig/build
 cmake ..

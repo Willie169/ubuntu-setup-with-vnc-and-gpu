e7903c9fb613edfb7f91cd281a266509053ed412
Willie Shen <cg10250207@gmail.com>
2026-02-26 15:57:28 +0800

    s

--- xmrig-install.sh
+++ xmrig-install.sh
@@ -2,8 +2,8 @@ cd ~
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev tor -y
 git clone https://github.com/xmrig/xmrig.git
 sed -i -e 's/keccak_f800_round(uint32_t st\[25\], const int r)/keccak_f800_round(__private uint32_t *st, const int r)/' -e 's/keccak_f800(uint32_t\* st)/keccak_f800(__private uint32_t *st)/' ~/xmrig/src/backend/opencl/cl/kawpow/kawpow.cl
-mkdir xmrig/build
-cd xmrig/build
+mkdir ~/xmrig/build
+cd ~/xmrig/build
 cmake ..
 make -j$(nproc)
 curl -fsSL https://raw.githubusercontent.com/xmrig/xmrig/refs/heads/master/scripts/randomx_boost.sh | sudo sh

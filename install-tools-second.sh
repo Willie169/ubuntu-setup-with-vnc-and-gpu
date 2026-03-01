403e805ed89b6f19ececa80c02957f11aa1bedb2
Willie169 <133617026+Willie169@users.noreply.github.com>
2026-03-01 23:36:25 +0800

    Update install-tools-second.sh

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,11 +1,6 @@
 cd ~
 while true; do sudo -v; sleep 60; done & SUDOPID=$!
 flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk org.gimp.GIMP org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.videolan.VLC -y
-sudo apt update
-sudo apt install -f -y
-sudo apt upgrade -y
-sudo apt autoremove --purge -y
-sudo apt clean
 flatpak update -y
 kill "$SUDOPID"
 exit

1bc8a2d24d2199f68e313647ad0134a7d63abc6a
Willie169 <cg10250207@gmail.com>
2026-03-02 16:29:44 +0800

    a

--- virtualgl-turbovnc.sh
+++ virtualgl-turbovnc.sh
@@ -1,16 +1,16 @@
 cd ~
 sudo apt update
 sudo apt install wget -y
-wget -q https://launchpad.net/ubuntu/+archive/primary/+files/libegl1-mesa_23.0.4-0ubuntu1~22.04.1_amd64.deb
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libegl1-mesa_23.0.4-0ubuntu1~22.04.1_amd64.deb
 sudo apt install ./libegl1-mesa_23.0.4-0ubuntu1~22.04.1_amd64.deb
 rm libegl1-mesa_23.0.4-0ubuntu1~22.04.1_amd64.deb
-wget -q https://sourceforge.net/projects/virtualgl/files/3.1/virtualgl_3.1_amd64.deb
+wget https://sourceforge.net/projects/virtualgl/files/3.1/virtualgl_3.1_amd64.deb
 sudo apt install ./virtualgl_3.1_amd64.deb
 rm virtualgl_3.1_amd64.deb
-wget -q https://sourceforge.net/projects/turbovnc/files/3.1/turbovnc_3.1_amd64.deb
+wget https://sourceforge.net/projects/turbovnc/files/3.1/turbovnc_3.1_amd64.deb
 sudo apt install ./turbovnc_3.1_amd64.deb
 rm turbovnc_3.1_amd64.deb
-wget -q https://sourceforge.net/projects/libjpeg-turbo/files/3.0.1/libjpeg-turbo-official_3.0.1_amd64.deb
+wget https://sourceforge.net/projects/libjpeg-turbo/files/3.0.1/libjpeg-turbo-official_3.0.1_amd64.deb
 sudo apt install ./libjpeg-turbo-official_3.0.1_amd64.deb
 rm libjpeg-turbo-official_3.0.1_amd64.deb
 sudo apt install dbus-x11 libglu1-mesa mesa-utils -y

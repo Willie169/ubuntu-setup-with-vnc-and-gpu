64212672a0b06153d4f7fc4e41b73ba619b1470b
Willie169 <cg10250207@gmail.com>
2026-03-03 13:06:07 +0800

    l

--- cuda.sh
+++ cuda.sh
@@ -4,8 +4,9 @@ echo y | sudo ubuntu-drivers install || true
 echo y | sudo ubuntu-drivers autoinstall || true
 echo y | sudo ubuntu-drivers autoinstall || true
 echo y | sudo ubuntu-drivers autoinstall || true
-sudo apt install wget -y
+sudo apt install aptitude wget -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt install ./cuda-keyring_1.1-1_all.deb -y
 sudo apt update
 sudo apt install cuda-toolkit -y
+sudo aptitude safe-upgrade --full-resolver -y

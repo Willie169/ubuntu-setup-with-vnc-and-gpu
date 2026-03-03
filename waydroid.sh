0586439e6d1f3418dd30ec6385d00f1995f4694c
Willie169 <cg10250207@gmail.com>
2026-03-03 14:00:43 +0800

    y

--- waydroid.sh
+++ waydroid.sh
@@ -1,14 +1,14 @@
 cd ~
 sudo apt update
-sudo apt install curl ca-certificates -y
-wget -q http://ftp.us.debian.org/debian/pool/main/l/lxc/liblxc1t64_6.0.5-1_amd64.deb
-sudo dpkg -i liblxc1t64_6.0.5-1_amd64.deb
+sudo apt install curl ca-certificates wget -y
+wget http://ftp.us.debian.org/debian/pool/main/l/lxc/liblxc1t64_6.0.5-1_amd64.deb
+sudo apt install ./liblxc1t64_6.0.5-1_amd64.deb -y
 rm liblxc1t64_6.0.5-1_amd64.deb
-wget -q http://ftp.us.debian.org/debian/pool/main/l/lxc/liblxc-common_6.0.5-1_amd64.deb
-sudo dpkg -i liblxc-common_6.0.5-1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/l/lxc/liblxc-common_6.0.5-1_amd64.deb
+sudo apt install ./liblxc-common_6.0.5-1_amd64.deb -y
 rm liblxc-common_6.0.5-1_amd64.deb
-wget -q http://ftp.us.debian.org/debian/pool/main/l/lxc/lxc_6.0.5-1_amd64.deb
-sudo dpkg -i lxc_6.0.5-1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/l/lxc/lxc_6.0.5-1_amd64.deb
+sudo apt install ./lxc_6.0.5-1_amd64.deb -y
 rm lxc_6.0.5-1_amd64.deb
 curl -s https://repo.waydro.id | sudo bash
 sudo apt update

58780d343902e366ad2a8d9cfb2ddcc76bc1e4ef
Willie169 <cg10250207@gmail.com>
2026-03-03 13:47:19 +0800

    k

--- cuda.sh
+++ cuda.sh
@@ -1,11 +1,12 @@
-sudo apt autoremove --purge *nvidia* cuda* libcublas-* libcublas-dev-* libcufile-* libcuobjclient-* libcurand-* libcurand-dev-* libcusolver-* libcusolver-dev-* libcusparse-* libcusparse-* libcusparse-dev-* libnpp-* libnpp-dev-* libnvfatbin-* libnvjitlink-* libnvvm-* libcufft-* libcufft-dev-* libcufile-dev-* libcuobjclient-dev-* libnvfatbin-dev-* libnvjitlink-dev-* libnvjpeg-* libnvjpeg-dev-* -y
-echo y | sudo ubuntu-drivers install || true
-echo y | sudo ubuntu-drivers install || true
-echo y | sudo ubuntu-drivers install || true
-echo y | sudo ubuntu-drivers autoinstall || true
-echo y | sudo ubuntu-drivers autoinstall || true
-echo y | sudo ubuntu-drivers autoinstall || true
 sudo apt install wget -y
+sudo apt remove --purge ^.*nvidia.* ^cuda.* -y
+sudo apt autoremove
+echo y | sudo ubuntu-drivers install || true
+echo y | sudo ubuntu-drivers install || true
+echo y | sudo ubuntu-drivers install || true
+echo y | sudo ubuntu-drivers autoinstall || true
+echo y | sudo ubuntu-drivers autoinstall || true
+echo y | sudo ubuntu-drivers autoinstall || true
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo apt install ./cuda-keyring_1.1-1_all.deb -y
 rm cuda-keyring_1.1-1_all.deb

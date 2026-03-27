4a511f7412fe108ef94c658d7dd9382dddc1d808
Willie169 <cg10250207@gmail.com>
2026-03-27 10:19:11 +0800

    y

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,2 +1,2 @@
-flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk org.gimp.GIMP org.gnome.SimpleScan org.kde.kdenlive org.kde.krita org.musescore.MuseScore org.onlyoffice.desktopeditors org.videolan.VLC -y
+flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk org.gimp.GIMP org.gnome.SimpleScan org.kde.kdenlive org.kde.krita org.kde.tokodon org.musescore.MuseScore org.onlyoffice.desktopeditors org.videolan.VLC -y
 flatpak update -y

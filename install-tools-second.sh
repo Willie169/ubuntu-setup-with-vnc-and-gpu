32cc7c257d860bc6a8ef6712d650a885938cf879
Willie169 <cg10250207@gmail.com>
2026-04-01 20:38:07 +0800

    l

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,4 +1,4 @@
-flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk io.github.nozwock.Packet org.gimp.GIMP org.gnome.SimpleScan org.kde.alligator org.kde.kdenlive org.kde.krita org.kde.tokodon org.musescore.MuseScore org.onlyoffice.desktopeditors org.videolan.VLC -y
+flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.github.BrisklyDev.Brisk org.gimp.GIMP org.gnome.SimpleScan org.kde.alligator org.kde.kdenlive org.kde.krita org.kde.tokodon org.localsend.localsend_app org.musescore.MuseScore org.onlyoffice.desktopeditors org.videolan.VLC -y
 flatpak update -y
-sudo ufw allow 9300
+sudo ufw allow 53317
 sudo ufw reload

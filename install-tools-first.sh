df8a850f7aa950051764b9f510403d8c329b8352
Willie169 <cg10250207@gmail.com>
2026-04-01 12:05:15 +0800

    l

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -413,8 +413,8 @@ sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 sudo apt update
 sudo apt install element-desktop -y </dev/null
-wget --tries=100 --retry-connrefused --waitretry=5 https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.1.8/android-studio-panda1-patch1-linux.tar.gz
-sudo tar -xzf android-studio-panda1-patch1-linux.tar.gz -C /opt/
+wget --tries=100 --retry-connrefused --waitretry=5 https://edgedl.me.gvt1.com/android/studio/ide-zips/2025.3.2.6/android-studio-panda2-linux.tar.gz
+sudo tar -xzf android-studio-*-linux.tar.gz -C /opt/
 rm android-studio-panda1-patch1-linux.tar.gz
 echo '[Desktop Entry]
 Version=1.0

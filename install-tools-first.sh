c514450767d65cb0bbd1ff5f7d5924fc737b253e
Willie169 <cg10250207@gmail.com>
2026-04-07 18:52:46 +0800

    l

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -248,6 +248,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
+corepack enable npm
 corepack enable yarn
 corepack enable pnpm
 npm install jsdom markdown-toc marked marked-gfm-heading-id node-html-markdown showdown

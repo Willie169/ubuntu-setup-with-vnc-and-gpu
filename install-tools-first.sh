467058f4b39216ad5d6b182d9d1dcdc77e10be98
Willie169 <cg10250207@gmail.com>
2026-04-10 16:33:04 +0800

    y

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -254,7 +254,7 @@ corepack enable pnpm
 npm install jsdom markdown-toc marked marked-gfm-heading-id node-html-markdown showdown
 npm install -g bash-language-server dockerfile-language-server-nodejs http-server pyright tree-sitter-cli
 curl --retry 100 --retry-connrefused --retry-delay 5 -fsSL https://bun.com/install | bash
-pipx install cmake-language-server libretranslate notebook jupyterlab jupytext meson poetry pylatexenc uv
+pipx install cmake-language-server gh2md libretranslate notebook jupyterlab jupytext meson poetry pylatexenc uv
 cat > ~/.config/systemd/user/libretranslate.service <<EOF
 [Unit]
 Description=LibreTranslate

df4d49bd35643ef028bc801162b7f2e7eb78c3ab
Willie169 <cg10250207@gmail.com>
2026-05-24 10:55:05 +0800

    Revert "y"
    This reverts commit e58742925a203d5a9b85137511960979d182d3d6.

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1883,13 +1883,5 @@ sudo apt upgrade -y
 sudo apt autoremove --purge -y
 sudo apt clean
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.gitlab.news_flash.NewsFlash org.gimp.GIMP org.gnome.SimpleScan org.kde.kdenlive org.kde.krita org.kde.tokodon org.localsend.localsend_app org.luanti.luanti org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
-flatpak install flathub com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.gitlab.news_flash.NewsFlash org.gimp.GIMP org.gnome.SimpleScan org.kde.kdenlive org.kde.krita org.kde.tokodon org.localsend.localsend_app org.luanti.luanti org.musescore.MuseScore org.onlyoffice.desktopeditors org.telegram.desktop org.videolan.VLC -y
-flatpak update -y
-gh_latest Elleo/pied com.mikeasoft.pied.flatpak
-sudo flatpak install com.mikeasoft.pied.flatpak -y
-rm com.mikeasoft.pied.flatpak*
-sudo ufw allow 53317
-sudo ufw reload
 kill "$SUDOPID"
 sudo reboot

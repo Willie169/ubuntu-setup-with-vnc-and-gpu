7afe0ea2bd4c478baef0511884b3658fdf23bcb0
Willie169 <cg10250207@gmail.com>
2026-06-23 12:23:08 +0800

    b

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -103,8 +103,8 @@ fi
 EOF
 sudo apt update
 sudo apt purge fcitx* texlive* yq -y
-sudo apt install apparmor-utils apt-transport-https build-essential ca-certificates cmake curl dbus openjdk-21-jdk dnscrypt-proxy g++ gcc git gnupg jq libeigen3-dev locales make maven ninja-build openssh-server perl perl-tk pipx python-is-python3 python3 qt6-base-dev qt6-base-dev-tools qt6-svg-dev qt6-5compat-dev wget -y
-sudo apt install clinfo ocl-icd-opencl-dev pipewire pipewire-audio-client-libraries podman uidmap wireplumber ufw unattended-upgrades -y
+sudo apt install apt-transport-https build-essential ca-certificates cmake curl dbus openjdk-21-jdk g++ gcc git gnupg jq libeigen3-dev locales make maven ninja-build openssh-server perl perl-tk pipx python-is-python3 python3 qt6-base-dev qt6-base-dev-tools qt6-svg-dev qt6-5compat-dev wget -y
+sudo apt install apparmor-utils clinfo dnscrypt-proxy ocl-icd-opencl-dev pipewire pipewire-audio-client-libraries podman uidmap wireplumber ufw unattended-upgrades -y
 rm -f .bashrc
 mkdir ~/.bashrc.d
 wget --tries=100 --retry-connrefused --waitretry=5 https://raw.githubusercontent.com/Willie169/bashrc/main/ubuntu-amd/bashrc.d/00-env.sh -O ~/.bashrc.d/00-env.sh

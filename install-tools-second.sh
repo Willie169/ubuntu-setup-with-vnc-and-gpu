9f739dbddb08cdc9621fa368203e762f6686ef64
Willie Shen <cg10250207@gmail.com>
2026-06-25 21:07:10 +0800

    a

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ~/.bashrc.d/50-functions.sh
 flatpak --user override --filesystem=xdg-config/fontconfig:ro
 flatpak install flathub com.github.vkohaupt.vokoscreenNG com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.gitlab.news_flash.NewsFlash me.timschneeberger.jdsp4linux org.freecad.FreeCAD org.gimp.GIMP org.kde.kdenlive org.kde.tokodon org.localsend.localsend_app org.luanti.luanti org.musescore.MuseScore org.telegram.desktop org.videolan.VLC -y
 flatpak update -y

b0aa5bcaa9f97bc1e2824826a936a7d95c06ccce
Willie Shen <cg10250207@gmail.com>
2026-06-26 21:19:22 +0800

    y

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+[ "${1:-}" = '--test' ] && TEST=1 || TEST=0
 # shellcheck disable=2155
 PREDF=$(df --output=used / | tail -n1 || true)
-source ~/.bashrc.d/50-functions.sh
+. <(curl -fsSL https://raw.githubusercontent.com/Willie169/bashrc/main/ubuntu-amd/bashrc.d/50-functions.sh)
 flatpak --user override --filesystem=xdg-config/fontconfig:ro
 flatpak install flathub com.github.vkohaupt.vokoscreenNG com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.gitlab.news_flash.NewsFlash me.timschneeberger.jdsp4linux org.freecad.FreeCAD org.gimp.GIMP org.kde.kdenlive org.kde.tokodon org.localsend.localsend_app org.luanti.luanti org.musescore.MuseScore org.telegram.desktop org.videolan.VLC -y
 flatpak update -y
 gh_latest -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' Elleo/pied com.mikeasoft.pied.flatpak
-sudo flatpak install com.mikeasoft.pied.flatpak -y
+flatpak install com.mikeasoft.pied.flatpak -y
 rm com.mikeasoft.pied.flatpak*
+if [ "$TEST" -eq 1 ]; then
 sudo ufw allow 53317
 sudo ufw reload
+fi
 # shellcheck disable=2155
 POSTDF=$(df --output=used / | tail -n1 || true)
 echo "PREDF: $PREDF"

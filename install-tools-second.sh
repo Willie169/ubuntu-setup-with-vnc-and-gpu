bbb8f8b76c0c2d35028ed1e47724db98a556458b
Willie Shen <cg10250207@gmail.com>
2026-06-27 01:59:17 +0800

    y

--- install-tools-second.sh
+++ install-tools-second.sh
@@ -7,7 +7,11 @@ flatpak --user override --filesystem=xdg-config/fontconfig:ro
 flatpak install flathub com.github.vkohaupt.vokoscreenNG com.usebottles.bottles fr.handbrake.ghb io.ente.auth io.freetubeapp.FreeTube io.gitlab.news_flash.NewsFlash me.timschneeberger.jdsp4linux org.freecad.FreeCAD org.gimp.GIMP org.kde.kdenlive org.kde.tokodon org.localsend.localsend_app org.luanti.luanti org.musescore.MuseScore org.telegram.desktop org.videolan.VLC -y
 flatpak update -y
 gh_latest -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' Elleo/pied com.mikeasoft.pied.flatpak
+if [ "$TEST" -eq 0 ]; then
+sudo flatpak install com.mikeasoft.pied.flatpak -y
+else
 flatpak install com.mikeasoft.pied.flatpak -y
+fi
 rm com.mikeasoft.pied.flatpak*
 if [ "$TEST" -eq 0 ]; then
 sudo ufw allow 53317

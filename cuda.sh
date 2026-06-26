311bdf9ca18fe649f9798dce429dda6050df95ad
Willie169 <133617026+Willie169@users.noreply.github.com>
2026-06-26 19:09:47 +0800

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~ || exit
 cat /etc/default/grub | grep GRUB_CMDLINE_LINUX_DEFAULT | grep -q 'nvidia_drm.modeset=1' && sudo sed -Ei "s/^GRUB_CMDLINE_LINUX_DEFAULT='(.*)'[ \t ]*/GRUB_CMDLINE_LINUX_DEFAULT='\1 nvidia_drm.modeset=1'/" /etc/default/grub
 sudo apt install wget -y
 sudo apt remove --purge ^.*nvidia.* ^cuda.* -y
@@ -15,3 +16,4 @@ rm cuda-keyring_1.1-1_all.deb*
 sudo apt update
 sudo apt install cuda-toolkit -y
 sudo apt autoremove -y
+sudo reboot

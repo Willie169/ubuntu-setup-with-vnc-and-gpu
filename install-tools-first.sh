fd321ca31aae46133e6bd017ca00b98a4b1013ac
Willie Shen <cg10250207@gmail.com>
2026-07-28 16:50:24 +0800

    l

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -123,6 +123,7 @@ sudo add-apt-repository ppa:flexiondotorg/quickemu -y
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:libreoffice/ppa -y
 sudo add-apt-repository ppa:longsleep/golang-backports -y
+sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo mv /etc/apt/sources.list.d/openjdk-r-ubuntu-ppa-*.sources /etc/apt/sources.list.d/openjdk-r-ubuntu-ppa-noble.sources || true

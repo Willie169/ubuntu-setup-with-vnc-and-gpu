8c726155eef4a3f58b9a66ec11a36a8d773043e1
Willie Shen <cg10250207@gmail.com>
2026-07-30 19:09:47 +0800

    l

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1453,7 +1453,7 @@ chmod +x yt-dlp
 mv yt-dlp ~/.local/bin/
 curl -LsSf https://astral.sh/uv/install.sh | sh
 if [ "$TEST" -eq 0 ]; then
-for pkg in cmake-language-server gallery-dl gh2md img2pdf jupyterlab jupytext libretranslate meson notebook pylatexenc tldr xmljson yamllint; do
+for pkg in autopep8 cmake-language-server gallery-dl gh2md img2pdf jupyterlab jupytext libretranslate meson notebook pylatexenc tldr xmljson yamllint; do
 uv tool install "$pkg"
 done
 uv tool install fdroidserver

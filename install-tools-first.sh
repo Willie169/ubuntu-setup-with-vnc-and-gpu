bae0850aa6bce2a0a7418ecd1464e484f0d2ae79
Willie Shen <cg10250207@gmail.com>
2026-08-09 22:08:19 +0800

    2026-08-09T14:08:19+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1284,7 +1284,7 @@ https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro-onlydomain
 https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.medium-onlydomains.txt
 EOF
 sudo tee /usr/share/dnscrypt-proxy/utils/generate-domains-blocklist/domains-allowlist.txt >/dev/null <<'EOF'
-# domain allow list
+# domains allow list
 EOF
 sudo tee /etc/systemd/system/dnscrypt-proxy-blocklist-update.service >/dev/null <<'EOF'
 [Unit]

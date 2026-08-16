ad4095aa74979374c42fc055c4508718a04a6f82
Willie Shen <cg10250207@gmail.com>
2026-08-17 01:04:00 +0800

    2026-08-16T17:04:00+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1437,15 +1437,15 @@ echo y | corepack enable npm
 echo y | npm --help || true
 echo y | corepack enable yarn
 echo y | yarn --help || true
-NPMG='deno http-server prettier'
-NPMIGNORE='opencode-ai @earendil-works/pi-coding-agent'
+NPMGALLOW='deno http-server prettier'
+NPMGIGNORE='opencode-ai @earendil-works/pi-coding-agent'
 # shellcheck disable=2086
 if [ "$TEST" -eq 0 ]; then
-	npmig $NPMG
-	npm i -g --ignore-scripts $NPMIGNORE
+	npmig $NPMGALLOW
+	npm i -g --ignore-scripts $NPMGIGNORE
 else
-	npmig -o --dry-run $NPMG
-	npm i -g --ignore-scripts --dry-run $NPMIGNORE
+	npmig -o --dry-run $NPMGALLOW
+	npm i -g --ignore-scripts --dry-run $NPMGIGNORE
 fi
 gh_release -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' yt-dlp/yt-dlp yt-dlp
 chmod +x yt-dlp

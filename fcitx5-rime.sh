7b92650131f682185ce8b1da72e92272aa881699
Willie Shen <cg10250207@gmail.com>
2026-08-21 23:23:09 +0800

    2026-08-21T15:23:09+00:00

--- fcitx5-rime.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo DEBIAN_FRONTEND=noninteractive apt install fcitx5 fcitx5-configtool fcitx5-frontend-all fcitx5-rime -y -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-overwrite"

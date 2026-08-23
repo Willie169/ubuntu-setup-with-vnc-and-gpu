dbdbab44cd57f1c3c635d13d15d3ece553f44a7a
Willie Shen <cg10250207@gmail.com>
2026-08-23 14:14:25 +0800

    2026-08-23T06:14:25+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1486,7 +1486,7 @@ conda config --add channels pytorch
 conda config --add channels conda-forge
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-BREW='bat bottom broot dust fd ffmpeg-full fzf git-delta lazygit procs resvg ripgrep sevenzip yazi yq zoxide'
+BREW='bat bottom broot dust fd ffmpeg-full fzf git-delta lazygit procs resvg ripgrep sevenzip vgmstream yazi yq zoxide'
 CASK='altersend'
 if [ "$TEST" -eq 0 ]; then
   # shellcheck disable=2086

17f470a758263476ea17b178eaf06d876d47c2a1
Willie Shen <cg10250207@gmail.com>
2026-08-27 20:19:09 +0800

    2026-08-27T12:19:09+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1486,7 +1486,7 @@ git config --global pull.rebase true
 git config --global init.defaultBranch main
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-BREW='bat bottom broot dust fd ffmpeg-full fzf git-delta hugo lazygit procs resvg ripgrep sevenzip vgmstream yazi yq zoxide'
+BREW='bat bottom broot dust fd ffmpeg fzf git-delta hugo lazygit procs resvg ripgrep sevenzip vgmstream yazi yq zoxide'
 CASK='altersend'
 if [ "$TEST" -eq 0 ]; then
   # shellcheck disable=2086
